fe1719738943941d8422bc166b94ca3280244484
DuffyWang926 <695944018@qq.com>
2026-02-05 15:30:55 +0800

    add google

--- src/components/HeadScripts.js
+++ src/components/HeadScripts.js
@@ -25,13 +25,16 @@ export default function HeadScripts() {
         }}
       />
       <script async src="https://www.googletagmanager.com/gtag/js?id=G-Z7N7WFV18G "></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments)}
-        gtag('js', new Date());
-
-        gtag('config', 'G-Z7N7WFV18G');
-      </script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments)}
+            gtag('js', new Date());
+            gtag('config', 'G-Z7N7WFV18G');
+          `,
+        }}
+      />
     </>
   );
 }
